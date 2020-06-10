8578f6b3a63c2d2fe477dd337e875587c01edd33
alex.kuhn <alexander.kuhn@freyasystems.com>
2020-06-10 13:14:09 -0400

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,6 +3,9 @@ import React, {Component} from 'react';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
+    constructor(props){
+        super(props)
+    }
     state = {
         quiz_position : 1
     }
